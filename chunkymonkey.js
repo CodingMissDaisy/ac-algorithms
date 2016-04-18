cddbfef012f5fa247800f707247eb174920d7738
D-saster100 <dakirahjenkins@Dakirahs-MacBook-Pro.local>
2016-04-18 08:56:18 -0700

    my answer to chunkymonkey.js

--- chunkymonkey.js
+++ chunkymonkey.js
@@ -15,9 +15,18 @@
 //chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
 
 
+//split entire array into groups the length of size
+//push arrays into an array
 function chunkArrayInGroups(arr, size) {
-  // Break it up.
-  return arr;
+ var array = [];
+  for (var i = 0; i < arr.length; i+=size) {
+    //split an array into groups of given size
+    var temparray = arr.slice(i,i+size);
+  	array.push(temparray);
+  }
+    console.log(array);
+
+  return array;
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups(["a", "b", "c", "d", "e"], 2);
