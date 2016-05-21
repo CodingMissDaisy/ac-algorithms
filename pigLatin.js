e7d4b6eddd544ebf639478b9a0fc579ccc8a5662
D-saster100 <dakirahjenkins@Dakirahs-MacBook-Pro.local>
2016-05-21 12:14:07 -0700

    my answer to the pigLatin problem

--- pigLatin.js
+++ pigLatin.js
@@ -6,16 +6,40 @@
 
 //If a word begins with a vowel you just add "way" to the end.
 
+//Input = string
+//Output = string translated to piglatin
 
 function translatePigLatin(str) {
-  //your code here
+	
+	var pigLatin = str.split("");
+  //check if the first letter is a constonant or vowel
+  switch(pigLatin[0]){
+  case "a" :
+  case "e" :
+  case "i" :
+  case "o" : 
+  case "u" :
+  //if first letter is a vowel, just add "way" to the end of the word 
+  //and join together
+  	pigLatin.push("way");
+    pigLatin = pigLatin.join("");
+    break;
+  // if first letter is a constonant take the first letter and push it to the end
+  //and then push "ay" to the end
+  // join together
+  default :
+  	pigLatin.push(pigLatin.shift());
+    pigLatin.push("ay");
+    pigLatin = pigLatin.join("");
+    break;
+  }
+  console.log(pigLatin);
 }
 
 translatePigLatin("consonant");
-
-// TEST CASES
-// translatePigLatin("california") should return "aliforniacay".
-// translatePigLatin("paragraphs") should return "aragraphspay".
-// translatePigLatin("glove") should return "oveglay".
-// translatePigLatin("algorithm") should return "algorithmway".
-// translatePigLatin("eight") should return "eightway".
+//TEST CASES
+translatePigLatin("california"); //should return "aliforniacay".
+translatePigLatin("paragraphs"); //should return "aragraphspay".
+translatePigLatin("glove"); //should return "oveglay".
+translatePigLatin("algorithm"); //should return "algorithmway".
+translatePigLatin("eight"); //should return "eightway".
