21d4ffeedb1e415123dfbc32f7345f261229a1cb
D-saster100 <dakirahjenkins@Dakirahs-MacBook-Pro.local>
2016-05-31 09:55:47 -0700

    My answer to the starMapping problem

--- starMapping.js
+++ starMapping.js
@@ -12,15 +12,34 @@
 
 //Helpful Link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/pow
 
+
+//Input = An object with two key/value pairs, name and avgAlt
+//Output = An object with two key/value pairs, name and orbital period.
+
 function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
-  //your code here
+  var pi = 2 * Math.PI;
+ //function for orbital period calculations
+  var getOrb = function(obj){
+ 	//Calculating the semi-major axis
+  	 var a = Math.pow(earthRadius + obj.avgAlt, 3); 
+  	 //Calculating the orbital period
+  	 var T = Math.round(Math.sqrt(a/GM) * pi);
+  	 //Removing avgAlt from all objects
+  	 delete obj.avgAlt;
+  	 //Adding the orbitalPeriod of each object
+  	 obj.orbitalPeriod = T;
+  };
+  //Calling the getOrb function for each object
+	for (var i = 0; i < arr.length; i++){
+		getOrb(arr[i]);
+  	}
+  console.log(arr);
 }
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
 
 //TEST CASES
-
-//orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]) should return [{name: "sputnik", orbitalPeriod: 86400}].
-//orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]) should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
+//orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]); //should return [{name: "sputnik", orbitalPeriod: 86400}].
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]); //should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
